e4fb1aca08adf5ff4fa2b867c4adbc7a270c18fc
Lily Chen <lilyychenn8@gmail.com>
2023-10-18 23:43:27 -0700

    Finished(???)

--- Source/prefabs/Rocket.js
+++ Source/prefabs/Rocket.js
@@ -5,6 +5,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         //add object to existing scene
         scene.add.existing(this);
+        this.isFiring = false;
+        this.moveSpeed = 2;
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update(){
@@ -16,8 +19,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
         //if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
@@ -29,5 +33,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.y = game.config.height - borderUISize - borderPadding;
         }
         
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
